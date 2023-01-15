b14ba8ca67385522d8d8dcf82a8fd1a3a89cb9c3
Your Name <you@example.com>
2023-01-15 21:54:58 +0400

    first-commit

---
+++ src/bean/Main.java
@@ -0,0 +1,16 @@
+package bean;
+
+import services.MenuLoginService;
+
+import java.util.Scanner;
+
+public class Main  {
+    public static void main(String[] args) {
+        MenuLoginService mls = new MenuLoginService();
+        mls.checkUser();
+    }
+
+
+
+
+}
